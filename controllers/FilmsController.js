cacf3a49b1ec257b1850a8ea5349cb34fdbbb771
Estlib <75094683+Estlib@users.noreply.github.com>
2025-12-01 18:02:49 +0200

    [Delivers #435862] As an app developer I want to get full details of a given <Game> to show up in my app.

--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -1,6 +1,7 @@
 
 const {db} = require('../db')
 const Utilities = require('./Utilities')
+const UUID = require('uuid')
 
 exports.getAll =
 async (req,res) => {
@@ -9,4 +10,36 @@ async (req,res) => {
     res
     .status(200)
     .send(films.map(({FilmID,Name}) => {return{FilmID,Name}}))
+}
+
+exports.getByID =
+async (req, res) => {
+    const film = await getFilm(req, res);
+    if (!film) 
+        {return res.status(404).send({error: 'Film not found'})}
+    return res.status(200).send(film)
+}
+
+const getFilm =
+async (req, res) => {
+    const idNumber = req.params.FilmID;
+    console.log(idNumber)
+    // if(!idNumber) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }    
+    // if(isNaN(idNumber)) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }
+    // if(UUID.validate(idNumber) !== true) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }
+    const film = await db.films.findByPk(idNumber);
+    if(!film) {
+        res.status(404).send({error: `Film with this id was not found ${idNumber}`})
+        return null;
+    }
+    return film;
 }
--- routes/ksRoutes.js
+++ routes/ksRoutes.js
@@ -4,4 +4,6 @@ const FilmsController = require("../controllers/FilmsController")
 module.exports = (app) => {
     app.route("/films")
     .get(FilmsController.getAll)
+    app.route("/films/:FilmID")
+    .get(FilmsController.getByID)
 }
