75b66adcac959aeeeba57babaf223225222be716
Estlib <75094683+Estlib@users.noreply.github.com>
2025-12-02 12:41:17 +0200

    As an app developer I want to get a list of all <Films> to show in my app. #435858

--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -3,3 +3,12 @@ const {db} = require('../db')
 const Utilities = require('./Utilities')
 const UUID = require('uuid')
 
+exports.getAll =
+async (req,res) => {
+    const films = await db.films.findAll();
+    console.log("getAll: "+films )
+    res
+    .status(200)
+    .send(films.map(({FilmID,Name}) => {return{FilmID,Name}}))
+}
+
