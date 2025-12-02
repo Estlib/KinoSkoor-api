acac7cf504e96c108bb4d9363ecd88a1a8791d2d
Estlib <75094683+Estlib@users.noreply.github.com>
2025-12-02 12:52:02 +0200

    #435862 As an app developer I want to get full details of a given <Film> to show up in my app.

--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -12,3 +12,34 @@ async (req,res) => {
     .send(films.map(({FilmID,Name}) => {return{FilmID,Name}}))
 }
 
+exports.getByID =
+async (req, res) => {
+    const film = await getFilm(req, res);
+    if (!film) 
+        {return res.status(404).send({error: 'Film not found'})}
+    return res.status(200).send(film)
+}
+
+const getFilm =
+async (req, res) => {
+    const idNumber = req.params.FilmID;
+    console.log(idNumber)
+    // if(!idNumber) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }    
+    // if(isNaN(idNumber)) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }
+    // if(UUID.validate(idNumber) !== true) {
+    //     res.status(400).send({error:`Entered id is not valid ${idNumber}`})
+    //     return null;
+    // }
+    const film = await db.films.findByPk(idNumber);
+    if(!film) {
+        res.status(404).send({error: `Film with this id was not found ${idNumber}`})
+        return null;
+    }
+    return film;
+}
