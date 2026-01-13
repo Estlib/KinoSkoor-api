b869f942353ae685d046241051cec73b7fb70ee0
Estlib <75094683+Estlib@users.noreply.github.com>
2026-01-13 16:30:48 +0200

    440384 As an app developer, I want to get a new valid Session, for any User that logs in.
    Delivers Task 400 & Task 200

--- backend-api/routes/ksRoutes.js
+++ backend-api/routes/ksRoutes.js
@@ -1,6 +1,7 @@
 
 const FilmsController = require("../controllers/FilmsController")
 const UsersController = require("../controllers/UsersController")
+const SessionsController = require("../controllers/SessionsController")
 
 module.exports = (app) => {
     app.route("/films")
@@ -10,12 +11,17 @@ module.exports = (app) => {
     .get(FilmsController.getByID)
     .delete(FilmsController.deleteById)
     .put(FilmsController.modifyById)
+
     app.route("/users")
     .get(UsersController.getAllUsers)
     .post(UsersController.create)
     app.route("/users/:UserID")
     .get(UsersController.getByID)
+
     app.route("/auth/:LoginEmail")
     .get(UsersController.getByEmail)
+    app.route("/session")
+    .post(SessionsController.newSession)
+
 
 }
